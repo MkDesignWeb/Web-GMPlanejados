08acf2c3ecabf179856b94bd2b8ca3669ff6a469
MKDesignWeb <99919855+mkdesignweb@users.noreply.github.com>
2022-03-06 22:48:12 -0300

    blocos de projetos

--- JS/script.js
+++ JS/script.js
@@ -3,4 +3,20 @@ $(".menu-mb").click(function () {
     $(this).toggleClass("active")
     $(".nav-list").toggleClass("active")
 });
+
+$(".bnt-menu").click(function () {
+    $(".menu-mb").toggleClass("active")
+    $(".nav-list").toggleClass("active")
 });
+
+});
+
+window.addEventListener('scroll', function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+    
+})
+
+
+
+
