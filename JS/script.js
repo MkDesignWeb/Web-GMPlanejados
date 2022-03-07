8752e60d9229a806214cfe69b20751cd2a63e3ef
MKDesignWeb <99919855+mkdesignweb@users.noreply.github.com>
2022-03-07 19:45:38 -0300

    update roda pé e modal

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,27 @@ window.addEventListener('scroll', function(){
     header.classList.toggle("sticky", window.scrollY > 0);
     
 })
+let imagens = document.querySelectorAll('.foto')
+let bfoto = document.querySelectorAll('b-foto')
+let modal = document.querySelector('.modal')
+let modalimg = document.querySelector('.img-modal')
+let bntclose = document.querySelector('#bnt-close')
+let srcVal="";
+
+
+for(let i =0; i<imagens.length;i++){
+    imagens[i].addEventListener('click',function(){
+        srcVal = imagens[i].getAttribute('src');
+        modalimg.setAttribute('src', srcVal);
+        modal.classList.toggle('modal-active');
+    });
+}
+
+bntclose.addEventListener('click', function(){
+    modal.classList.toggle('modal-active');
+})
+
+
 
 
 
