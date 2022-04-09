9da0acf1ed508a6e92f75981b8f42076f8153766
MkDesignWeb <99919855+MkDesignWeb@users.noreply.github.com>
2022-04-09 06:23:38 -0300

    Titulo e descrição no modal

--- JS/script.js
+++ JS/script.js
@@ -16,28 +16,56 @@ window.addEventListener('scroll', function(){
     header.classList.toggle("sticky", window.scrollY > 0);
     
 })
+let fmodal = document.querySelector('.fundom')
 let imagens = document.querySelectorAll('.foto')
-let bfoto = document.querySelectorAll('b-foto')
+let bfoto = document.querySelectorAll('.b-foto')
 let modal = document.querySelector('.modal')
 let modalimg = document.querySelector('.img-modal')
 let bntclose = document.querySelector('#bnt-close')
 let srcVal="";
+let titulonome = document.querySelectorAll('.texth2')
+let tituloval = ""
+let txtmodal = document.querySelector('.txtmodal')
+let descnome = document.querySelectorAll('.textp')
+let descval = ""
+let descmodal = document.querySelector('.descmodal')
 
 
-for(let i =0; i<imagens.length;i++){
-    imagens[i].addEventListener('click',function(){
-        srcVal = imagens[i].getAttribute('src');
-        modalimg.setAttribute('src', srcVal);
-        modal.classList.toggle('modal-active');
-    });
+ for(let i =0; i<imagens.length;i++){
+     bfoto[i].addEventListener('click',function(){
+         srcVal = imagens[i].getAttribute('src');
+         modalimg.setAttribute('src', srcVal);
+         modal.classList.toggle('modal-active');
+         fmodal.classList.toggle('active')
+
+     });
+ }
+
+for(let a =0; a<titulonome.length;a++){
+    bfoto[a].addEventListener('click',function(){
+        tituloval = titulonome[a].innerHTML
+        txtmodal.innerHTML = tituloval 
+
+    })
+}
+
+for(let b =0; b<descnome.length;b++){
+    bfoto[b].addEventListener('click',function(){
+        descval = descnome[b].innerText
+        descmodal.innerHTML = descval 
+
+    })
 }
 
 bntclose.addEventListener('click', function(){
-    modal.classList.toggle('modal-active');
+    modal.classList.toggle('modal-active')
+    fmodal.classList.toggle('active')
 })
 
-
-
+fmodal.addEventListener('click', function(){
+    modal.classList.toggle('modal-active')
+    fmodal.classList.toggle('active')
+})
 
 
 
